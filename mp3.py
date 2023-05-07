206ab525382731ae815481e8325198579bc117b8
Flávio Pavim <flaviopavim@gmail.com>
2023-05-07 00:45:30 -0300

    Add files via upload

---
+++ mp3.py
@@ -0,0 +1,13 @@
+from gtts import gTTS
+
+texto="";
+with open('texto.txt', encoding='utf-8') as file:
+    texto = file.read()
+
+print(texto)
+    
+tts = gTTS(text=texto, lang='pt-br', slow=False)
+tts.save('audio.mp3')
+
+print('Áudio criado')
+
